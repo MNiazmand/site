7de898b11fabdfd86da235ef214d1e8c40eee638
MNiazmand <my.niazmand@gmail.com>
2012-11-08 22:24:14 +0330

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MNSite::Application.config.secret_token = '67cb2e6134d91547ace75b9fcc1d4d6ebb4b87d536e9640f3dd5e7d4e0bafb405a00db597c8ec14c8d664e7c529c18512a46d8437153a2a632e3f1ac52c8f5a2'
